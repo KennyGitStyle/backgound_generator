b307112a288be03213dcda6e33523d30ebe22bc4
Kenneth <kennethhendricks10@gmail.com>
2021-09-28 12:53:00 +0100

    Read Color

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let css = document.querySelector("h3");
-let color1 = document.querySelector(".color1");
-let color2 = document.querySelector(".color2");
-let color3 = document.querySelector(".color3");
-let body = document.getElementById("gradient");
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var color3 = document.querySelector(".color3");
+var body = document.getElementById("gradient");
 
 const setGradient = () => {
   body.style.background =
@@ -13,6 +13,8 @@ const setGradient = () => {
     ", " +
     color3.value +
     ")";
+
+  css.textContent = body.style.background + ";";
 };
 
 color1.addEventListener("input", setGradient);
